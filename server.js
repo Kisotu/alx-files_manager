9453e3e4f62138eaa93cd9e1af172d62422cc749
Oloishorua <jacoboloishorua@gmail.com>
2024-10-02 21:47:13 +0300

    set to listen on PORT

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ import express from 'express';
 import filemanRoutes from './routes';
 
 const server = express();
+const PORT = process.env.PORT || 5000;
 
 filemanRoutes(server);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default server;
