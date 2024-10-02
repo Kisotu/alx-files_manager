e3b3a9b05333d815cea615c6be37f372dec6071d
Oloishorua <jacoboloishorua@gmail.com>
2024-10-03 01:23:58 +0300

    task 3 - create a new user reformartted postNew function

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,28 +1,40 @@
 /* eslint-disable import/no-named-as-default */
+import sha1 from 'sha1';
+import Queue from 'bull';
 import dbClient from '../utils/db';
 
+const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
+
 export default class UsersController {
-  static async postNew(req, res) {
-    const { email, password } = req.body;
+  static postNew(request, response) {
+    const { email } = request.body;
+    const { password } = request.body;
+
     if (!email) {
-      res.status(400).json({ error: 'Missing email' });
-      res.end();
+      response.status(400).json({ error: 'Missing email' });
       return;
     }
     if (!password) {
-      res.status(400).json({ error: 'Missing password' });
-      res.end();
+      response.status(400).json({ error: 'Missing password' });
       return;
     }
-    const userExist = await dbClient.userExist(email);
-    if (userExist) {
-      res.status(400).json({ error: 'Already exist' });
-      res.end();
-      return;
-    }
-    const user = await dbClient.createUser(email, password);
-    const id = `${user.insertedId}`;
-    res.status(201).json({ id, email });
-    res.end();
+
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne(
+          {
+            email,
+            password: hashedPassword,
+          },
+        ).then((result) => {
+          response.status(201).json({ id: result.insertedId, email });
+          userQueue.add({ userId: result.insertedId });
+        }).catch((error) => console.log(error));
+      }
+    });
   }
 }
