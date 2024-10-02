b49c82ba46f8d5a382a2d4b425fbb4d19269ddef
Oloishorua <jacoboloishorua@gmail.com>
2024-10-03 01:41:11 +0300

    task 4 - authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -37,4 +37,10 @@ export default class UsersController {
       }
     });
   }
+
+  static async getMe(req, res) {
+    const { user } = req;
+
+    res.status(200).json({ email: user.email, id: user._id.toString() });
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const filemanRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+
   api.post('/users', UsersController.postNew);
+  api.get('/users/me', UsersController.getMe);
+
+  api.get('/connect', AuthController.getConnect);
+  api.get('/disconnect', AuthController.getDisconnect);
 };
 
 export default filemanRoutes;
