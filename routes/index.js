814a9647078e83c9cb7a769dbaff0fa99bea7032
Oloishorua <jacoboloishorua@gmail.com>
2024-10-03 02:10:16 +0300

    task 5 - create file in db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const filemanRoutes = (api) => {
   api.get('/status', AppController.getStatus);
@@ -11,6 +12,8 @@ const filemanRoutes = (api) => {
 
   api.get('/connect', AuthController.getConnect);
   api.get('/disconnect', AuthController.getDisconnect);
+
+  api.post('/files', FilesController.postUpload);
 };
 
 export default filemanRoutes;
