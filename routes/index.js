f2ef0ce128502257ca7c7835199a900c64016c5a
Oloishorua <jacoboloishorua@gmail.com>
2024-10-02 22:12:38 +0300

    task 3 - create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const filemanRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+  api.post('/users', UsersController.postNew);
 };
 
 export default filemanRoutes;
