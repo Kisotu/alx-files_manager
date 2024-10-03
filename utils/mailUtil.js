7dfcd06af7e99359a2922dcf0dbac75d2732a453
Oloishorua <jacoboloishorua@gmail.com>
2024-10-03 03:04:43 +0300

    task 9 - image thumbnails

---
+++ utils/mailUtil.js
@@ -0,0 +1,127 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import fs from 'fs';
+import readline from 'readline';
+import { promisify } from 'util';
+import mimeMessage from 'mime-message';
+import { gmail_v1 as gmailV1, google } from 'google-apis';
+
+const TOKEN_PATH = 'token.json';
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+async function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) {
+        console.error('Error retrieving access token', err);
+        return;
+      }
+      oAuth2Client.setCredentials(token);
+      writeFileAsync(TOKEN_PATH, JSON.stringify(token))
+        .then(() => {
+          console.log('Token stored to', TOKEN_PATH);
+          callback(oAuth2Client);
+        })
+        .catch((writeErr) => console.error(writeErr));
+    });
+  });
+}
+
+async function authorize(credentials, callback) {
+  const clientSecret = credentials.web.client_secret;
+  const clientId = credentials.web.client_id;
+  const redirectURIs = credentials.web.redirect_uris;
+  const oAuth2Client = new google.auth.OAuth2(
+    clientId,
+    clientSecret,
+    redirectURIs[0],
+  );
+  console.log('Client authorization beginning');
+  // Check if token stored
+  await readFileAsync(TOKEN_PATH)
+    .then((token) => {
+      oAuth2Client.setCredentials(JSON.parse(token));
+      callback(oAuth2Client);
+    }).catch(async () => getNewToken(oAuth2Client, callback));
+  console.log('Client authorization done');
+}
+
+function sendMailService(auth, mail) {
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  gmail.users.messages.send({
+    userId: 'me',
+    requestBody: mail,
+  }, (err, _res) => {
+    if (err) {
+      console.log(`The API returned an error: ${err.message || err.toString()}`);
+      return;
+    }
+    console.log('Message sent successfully');
+  });
+}
+
+export default class Mailer {
+  static checkAuth() {
+    readFileAsync('credentials.json')
+      .then(async (content) => {
+        await authorize(JSON.parse(content), (auth) => {
+          if (auth) {
+            console.log('Auth check was successful');
+          }
+        });
+      })
+      .catch((err) => {
+        console.log('Error loading client secret file:', err);
+      });
+  }
+
+  static buildMessage(dest, subject, message) {
+    const senderEmail = process.env.GMAIL_SENDER;
+    const msgData = {
+      type: 'text/html',
+      encoding: 'UTF-8',
+      from: senderEmail,
+      to: [dest],
+      cc: [],
+      bcc: [],
+      replyTo: [],
+      date: new Date(),
+      subject,
+      body: message,
+    };
+
+    if (!senderEmail) {
+      throw new Error(`Invalid sender: ${senderEmail}`);
+    }
+    if (mimeMessage.validMimeMessage(msgData)) {
+      const mimeMsg = mimeMessage.createMimeMessage(msgData);
+      return { raw: mimeMsg.toBase64SafeString() };
+    }
+    throw new Error('Invalid MIME message');
+  }
+
+  static sendMail(mail) {
+    readFileAsync('credentials.json')
+      .then(async (content) => {
+        await authorize(
+          JSON.parse(content),
+          (auth) => sendMailService(auth, mail),
+        );
+      })
+      .catch((err) => {
+        console.log('Error loading client secret file:', err);
+      });
+  }
+}
